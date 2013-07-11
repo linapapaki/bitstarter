e85ccbb8832f5ebec5d834330d6338ccad68cbf1
Ubuntu <ubuntu@ip-172-31-46-64.us-west-2.compute.internal>
2013-07-11 22:42:23 +0000

    updated grader.js

--- grader.js
+++ grader.js
@@ -2,48 +2,35 @@
 /*
 Automatically grade files for the presence of specified HTML tags/attributes.
 Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
+and basic DOM  
+  
  + cheerio
    - https://github.com/MatthewMueller/cheerio
    - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
+   - http://maxogden.com/scraping-with-node.  
  + commander.js
    - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-  
  + JSON
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_  
+
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
-var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
-};
-
+  
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
+    return cheerio.load(fs.readFileSync( htmlfile));
+}; 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
-};
-
+};  
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
@@ -54,21 +41,54 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     }
     return out;
 };
-
+  
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
-
+  
+var check_json_after_get = function(result, type) {
+    var checkJson = checkHtmlFile(program.file, program.checks)
+    console.log(JSON.stringify(checkJson, null, 4));
+};
+  
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    console.log('%s', instr);
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); 
+// http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    check_json_after_get(instr, 'file');
+};
+  
+var getUrl = function(apiurl) {
+    rest.get(apiurl).on('complete', function(result) {
+        if (result instanceof Error) {
+            console.log('Error: ' + result.message);
+            process.exit(1);
+        } else {
+            program.file = 'tmp.txt';
+            fs.writeFileSync( program.file, result.toString());
+            check_json_after_get(result, 'url');
+	}
+})};
+    
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', 
+    clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <html_file>', 'Link to a url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url) {
+        getUrl(program.url)
+    }  else { 
+        assertFileExists(program.file);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+ 
